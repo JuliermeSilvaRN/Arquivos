70ed71542add5901656f838a4d61e7dca360f16e
Julierme Silva <juliermern@gmail.com>
2023-11-19 12:31:33 -0300

    Exempo de desvios

---
+++ Treinamento/Exemplos/condicional.py
@@ -0,0 +1,17 @@
+idade = 19
+
+if idade >= 18:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
+print('Fim')
+
+media = 6.5
+
+if media >= 7:
+    print('Aprovado!')
+else:
+    if media > 6:
+        print('Recuperação!')
+    else:
+        print('Reprovado!')
