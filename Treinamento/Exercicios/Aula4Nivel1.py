fc6d3933344a6e4fad6f794f082097bbc6c942ce
Julierme Silva <juliermern@gmail.com>
2023-11-19 12:44:09 -0300

    Outra forma de resolver

--- Treinamento/Exercicios/Aula4Nivel1.py
+++ Treinamento/Exercicios/Aula4Nivel1.py
@@ -4,3 +4,13 @@ if numero > 100 and numero < 200:
     print("Numero {} dentro do intervalo entre 100 e 200".format(numero))
 else:
     print("Numero {} fora do intervalo entre 100 e 200".format(numero))
+
+numero = int(input("Digite um número: "))
+
+if numero >= 100:
+    if numero <= 200:
+        print("O valor está no intervalo entre 100 e 200")
+    else:
+        print("O valor não esta no intervalo, o valor é maior do que 200")
+else:
+    print("O valor não esta no intervalo, o valor é menor do que 100")
